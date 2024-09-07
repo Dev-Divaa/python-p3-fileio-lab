8782618c3976318572ef770e10502d4e35dde0bf
Centy26 <centrineatieno20@gmail.com>
2024-09-07 20:27:32 +0300

    file IO

--- lib/file_io.py
+++ lib/file_io.py
@@ -6,3 +6,12 @@ def append_file(file_name, append_content):
 
 def read_file(file_name):
     pass
+
+
+# use write_file function. 
+write_file(file_name="logfile", file_content="Log 1: 5 bananas added" )
+append_file(file_name="logfile", append_content="Log 2: 3 bananas subtracted")
+
+read_file(file_name="logfile")
+# Log 1: 5 bananas added
+# Log 2: 3 bananas subtracted
